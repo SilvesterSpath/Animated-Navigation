091d9c56424fdc4aa0ce8163a6d31c08c8e925a0
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-13 09:14:51 +0200

    remove clg

--- script.js
+++ script.js
@@ -3,24 +3,17 @@ const nav = document.getElementById('nav');
 const li = document.querySelectorAll('li');
 const ul = document.querySelector('ul');
 
-console.log(li);
-console.log(ul);
-console.log(nav.classList);
-
 toggle.addEventListener('click', () => {
   nav.classList.toggle('active');
   if (nav.classList != 'active') {
-    console.log('!active');
     delayClose();
   }
   if (nav.classList == 'active') {
-    console.log('active');
     delayOpen();
   }
 });
 
 function delayClose() {
-  console.log('close');
   li.forEach((i) => {
     i.style.transitionDelay = '0s';
   });
@@ -29,7 +22,6 @@ function delayClose() {
 }
 
 function delayOpen() {
-  console.log('open');
   li.forEach((i) => {
     i.style.transitionDelay = '0.6s';
   });
